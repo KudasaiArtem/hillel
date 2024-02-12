5279996a1b43d2a36658b206e75855b5351b7a9b
Kudasai <trololoshka050@gmail.com>
2024-02-12 23:40:02 +0200

    added changing stats on click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// конструктор
 function Cat(breed = "немає", age, gender, color, name, status = "не спить", picture) {
     this.breed = breed;
     this.age = age;
@@ -11,7 +11,7 @@ function Cat(breed = "немає", age, gender, color, name, status = "не сп
     // методи
     this.askToEat = function() {
         if (this.status === "не спить") {
-            return ("хочу їсти");
+            return this.status = "хочу їсти";
         }
     }
 
@@ -24,12 +24,45 @@ function Cat(breed = "немає", age, gender, color, name, status = "не сп
     }
 }
 
-const cat1 = new Cat(undefined, 1, "male", "чорний", "чебурек", undefined, "./cat_pic/cat1.jfif");
+// додаємо котів за допомогою конструктора
+const cat1 = new Cat(undefined, 1, "female", "біло-сірий", "біляш", undefined, "./cat_pic/cat1.jfif");
 
-console.log(cat1);
+// console.log(cat1);
 
-const cat2 = new Cat(undefined, 1, "male", "чорний", "чебурек", "./cat_pic/cat1.jfif", undefined);
+const cat2 = new Cat(undefined, 2, "male", "сірий", "мурчик", undefined, "./cat_pic/cat2.jpg");
 
-const cat3 = new Cat(undefined, 1, "male", "чорний", "чебурек", "./cat_pic/cat1.jfif", undefined);
+const cat3 = new Cat(undefined, 4, "male", "чорний", "чебурек", undefined, "./cat_pic/cat3.jpg");
 
+//масив із котів
 const cats = [cat1, cat2, cat3];
+
+
+const btn = document.querySelector(".button-block");
+
+// виводить данні про кота
+const showCat = function(cat) {
+    let picture = document.querySelector("img");
+
+    picture.src = cat.picture;
+
+    // document.querySelector("#breed").innerHTML = cat.breed;
+    document.querySelector("#breed").textContent = cat.breed;
+
+    document.querySelector("#age").textContent = cat.age;
+
+    document.querySelector("#gender").textContent = cat.gender;
+
+    document.querySelector("#color").textContent = cat.color;
+
+    document.querySelector("#name").textContent = cat.name;
+
+    document.querySelector("#status").textContent = cat.status;
+}
+
+
+// викликає функцію для відображання данних про кота
+btn.addEventListener("click", (event) => {
+    let cat_id = event.target.value;
+
+    showCat(cats[cat_id]);
+})

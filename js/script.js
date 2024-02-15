dbdf7cfbed9e9ab725b8af033b77015ccb5272e7
Kudasai <trololoshka050@gmail.com>
2024-02-15 20:10:40 +0200

    jquery

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready((e) => {
+    console.log("DOM is ready");
+})
+
+// 1
+$("#addHtmlt").html("<h1> New Element </h1>");
+
+// 2
+$(".someClass").hide();
+
+// 3
+$("section").remove();
+
+// 4
+
+$("div")
+    .addClass("addClass")
+    .text("Lumos Maxima")
+    .show();
+
